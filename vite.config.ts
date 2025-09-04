99d88f8295814133fa3ff14b2d1ec942b5172e0f
rajushah404 <rajushah2287@gmail.com>
2025-09-04 20:44:30 +0545

    t

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/raju-shah.github.io/',
+  base: '/',
   css: {
     postcss: './postcss.config.js'
   },
